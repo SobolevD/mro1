a2facb6e6e63b6c9bd5797bbfa1ae606b6102bc6
Administrator <admin@example.com>
2022-10-27 00:48:34 +0400

    Code refactoring

--- main.py
+++ main.py
@@ -2,43 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 
-from utils.binary import LETTER_C, LETTER_P, transform_matrix, transform_matrices, \
-    calculate_array_of_condition_probabilities, calculate_binary_SD, calculate_binary_m, calculate_lambda_tilda, \
-    classify_array_of_vectors, PROBABILITY_CLASS_C, PROBABILITY_CLASS_P, calculate_exp_error, \
+from utils.binary import LETTER_C, LETTER_P, transform_matrices, \
+    calc_condition_probs, calc_binarySD, calc_M, calc_small_lambda, \
+    classify_vectors_array, PROBABILITY_CLASS_C, PROBABILITY_CLASS_P, calculate_exp_error, \
     calculate_theoretical_errors
 
 if __name__ == '__main__':
 
-    result_1 = transform_matrix(LETTER_C, 0.3)
-    result_2 = transform_matrix(LETTER_P, 0.3)
+    P_change = 0.3
+    SET_SIZE = 200
+    LETTER_SHAPE = LETTER_C.shape
+
+    result_1 = transform_matrices(LETTER_C, 1)
+    result_2 = transform_matrices(LETTER_P, 1)
 
     figure = plt.figure(figsize=(10, 10))
     plt.title("Letters")
     sub_figure_1 = figure.add_subplot(2, 2, 1)
     plt.imshow(1 - LETTER_C, cmap='gray')
-    sub_figure_1.set_title("Буква П")
+    sub_figure_1.set_title("'Ц' letter")
 
     sub_figure_2 = figure.add_subplot(2, 2, 2)
     plt.imshow(1 - result_1, cmap='gray')
-    sub_figure_2.set_title("Буква П после обработки")
+    sub_figure_2.set_title("Processed 'Ц' letter")
 
     sub_figure_3 = figure.add_subplot(2, 2, 3)
     plt.imshow(1 - LETTER_P, cmap='gray')
-    sub_figure_3.set_title("Буква Б")
+    sub_figure_3.set_title("'P' letter")
 
     sub_figure_4 = figure.add_subplot(2, 2, 4)
     plt.imshow(1 - result_2, cmap='gray')
-    sub_figure_4.set_title("Буква Б после обработки")
+    sub_figure_4.set_title("Processed 'P' letter")
     plt.show()
 
-    test_data_class_p = transform_matrices(LETTER_C, 200, 0.3)
-    test_data_class_b = transform_matrices(LETTER_P, 200, 0.3)
+    test_data_class_p = transform_matrices(LETTER_C, SET_SIZE, P_change)
+    test_data_class_b = transform_matrices(LETTER_P, SET_SIZE, P_change)
 
-    cond_prob_array_class_p = calculate_array_of_condition_probabilities(test_data_class_p)
-    cond_prob_array_class_b = calculate_array_of_condition_probabilities(test_data_class_b)
+    cond_prob_array_class_p = calc_condition_probs(test_data_class_p)
+    cond_prob_array_class_b = calc_condition_probs(test_data_class_b)
 
-    sd0, sd1 = calculate_binary_SD(cond_prob_array_class_p, cond_prob_array_class_b)
-    m0, m1 = calculate_binary_m(cond_prob_array_class_p, cond_prob_array_class_b)
+    sd0, sd1 = calc_binarySD(cond_prob_array_class_p, cond_prob_array_class_b)
+    m0, m1 = calc_M(cond_prob_array_class_p, cond_prob_array_class_b)
     print("ME 0", m0)
     print("ME 1", m1)
     print("SD 0", sd0)
@@ -50,21 +54,21 @@ if __name__ == '__main__':
 
     x1 = np.arange(-25, 25, 0.001)
     plt.plot(x1, norm.pdf(x1, m1, sd1), color='red', linewidth=3)
-    lambda_tilda = calculate_lambda_tilda(0.5, 0.5, cond_prob_array_class_p, cond_prob_array_class_b)
+    lambda_tilda = calc_small_lambda(0.5, 0.5, cond_prob_array_class_p, cond_prob_array_class_b)
     array_lambda_tilda = np.zeros(4) + lambda_tilda
     plt.plot(array_lambda_tilda, np.arange(0, 0.2, 0.05), color='black')
     print("lambda_tilda: ", lambda_tilda)
     plt.show()
 
-    classified_array_class_p = classify_array_of_vectors(test_data_class_p, PROBABILITY_CLASS_C,
-                                                                         PROBABILITY_CLASS_P,
-                                                                         cond_prob_array_class_p,
-                                                                         cond_prob_array_class_b)
+    classified_array_class_p = classify_vectors_array(test_data_class_p, PROBABILITY_CLASS_C,
+                                                      PROBABILITY_CLASS_P,
+                                                      cond_prob_array_class_p,
+                                                      cond_prob_array_class_b)
 
-    classified_array_class_b = classify_array_of_vectors(test_data_class_b, PROBABILITY_CLASS_C,
-                                                                         PROBABILITY_CLASS_P,
-                                                                         cond_prob_array_class_b,
-                                                                         cond_prob_array_class_p)
+    classified_array_class_b = classify_vectors_array(test_data_class_b, PROBABILITY_CLASS_C,
+                                                      PROBABILITY_CLASS_P,
+                                                      cond_prob_array_class_b,
+                                                      cond_prob_array_class_p)
 
     class_p_exp_error = calculate_exp_error(classified_array_class_p)
     class_b_exp_error = calculate_exp_error(classified_array_class_b)
--- utils/binary.py
+++ utils/binary.py
@@ -1,8 +1,5 @@
 import math
 import numpy as np
-from skimage.io import show, imshow
-from matplotlib import pyplot as plt
-from scipy.special import erf
 
 from utils.laplas import laplas_function
 
@@ -33,145 +30,139 @@ PROBABILITY_CLASS_C = 0.5
 PROBABILITY_CLASS_P = 0.5
 
 
+# =============================
+# Основная работа с матрицами
+# =============================
 def invert_value(p, value, p_change=0.3):
     return value if p > p_change else 1 - value
 
 
-def transform_matrix(letter_matrix, p_change=0.3):
+def transform_matrices(letter_matrix, selection_size=200, p_change=0.3):
 
-    matrix_as_vector = letter_matrix.flatten()
     letter_shape = np.shape(letter_matrix)
-
-    random_vector_size = letter_shape[0] * letter_shape[1]
-    random_vector = np.random.uniform(0, 1, random_vector_size)
-
-    invert_matrix_values = np.vectorize(invert_value)
-    result = invert_matrix_values(random_vector, matrix_as_vector, p_change)
-
-    return np.reshape(result, letter_shape)
-
-
-def transform_matrices(letter_matrix, selection_size, p_probability_of_change):
-
-    shape = np.shape(letter_matrix)
     matrix_as_vector = letter_matrix.flatten()
-    result = np.zeros((selection_size, shape[0] * shape[1]))
+    result = np.zeros((selection_size, letter_shape[0] * letter_shape[1]))
 
     invert_matrix_values = np.vectorize(invert_value)
     for i in range(0, selection_size, 1):
-        random_vector = np.random.uniform(0, 1, shape[0] * shape[1])
-        result[i] = invert_matrix_values(random_vector, matrix_as_vector, p_probability_of_change)
-        # selection size strings AND 81 columns
+        random_vector = np.random.uniform(0, 1, letter_shape[0] * letter_shape[1])
+        result[i] = invert_matrix_values(random_vector, matrix_as_vector, p_change)
+
+    # Если выборка одна, просто вернем один массив
+    if selection_size == 1:
+        return result[0].reshape(letter_matrix.shape)
     return result
 
 
-def calculate_array_of_condition_probabilities(array_of_vectors_seed_class):
-    shape_of_array = np.shape(array_of_vectors_seed_class)
-    shape_result_array = np.shape(array_of_vectors_seed_class[0])
-    sum_of_vector_coordinates_values = np.zeros(shape_result_array)
+def calc_condition_probs(set_changed_vectors):
+
+    shape_of_array = np.shape(set_changed_vectors)  # 200 x 81
+    shape_result_array = np.shape(set_changed_vectors[0])
+
+    cum_sum = np.zeros(shape_result_array)
     for i in range(0, shape_of_array[0], 1):
-        sum_of_vector_coordinates_values += array_of_vectors_seed_class[i]
+        cum_sum += set_changed_vectors[i]
 
-    return np.divide(sum_of_vector_coordinates_values, shape_of_array[0])
+    print(f'CUMSUM: {cum_sum}')
+    return np.divide(cum_sum, shape_of_array[0])
 
 
-def calculate_binary_SD(array_of_condition_probabilities_omega_0, array_of_condition_probabilities_omega_1):
-    size = array_of_condition_probabilities_omega_0.size
-    result_0 = np.zeros(size)
-    result_1 = np.zeros(size)
+# =============================
+# Параметры бинарного распределения
+# =============================
+def calc_binarySD(cond_probs_array_0, cond_probs_array_1):
+    size = cond_probs_array_0.size
+
+    part_0 = np.zeros(size)
+    part_1 = np.zeros(size)
+
     for i in range(0, size, 1):
-        log_part = calculate_w_lj_coefficients_array(array_of_condition_probabilities_omega_1,
-                                                     array_of_condition_probabilities_omega_0)[i]
-        result_0[i] = np.power(log_part, 2) * array_of_condition_probabilities_omega_0[i] * (1 - array_of_condition_probabilities_omega_0[i])
-        result_1[i] = np.power(log_part, 2) * array_of_condition_probabilities_omega_1[i] * (1 - array_of_condition_probabilities_omega_1[i])
+        log_part = calc_Wlj_coef_arr(cond_probs_array_1, cond_probs_array_0)[i]
+        part_0[i] = np.power(log_part, 2) * cond_probs_array_0[i] * (1 - cond_probs_array_0[i])
+        part_1[i] = np.power(log_part, 2) * cond_probs_array_1[i] * (1 - cond_probs_array_1[i])
 
-    return np.sqrt(np.sum(result_0)), np.sqrt(np.sum(result_1))
-
-
-def calculate_w_lj_coefficients_array(array_of_condition_probabilities_omega_l,
-                                      array_of_condition_probabilities_omega_j):
-    # shape is (81)
-    shape = np.shape(array_of_condition_probabilities_omega_l)
-    result = np.zeros(shape)
-    for i in range(0, shape[0], 1):
-        result[i] = math.log(
-            ((array_of_condition_probabilities_omega_l[i] / (1 - array_of_condition_probabilities_omega_l[i])) *
-             ((1 - array_of_condition_probabilities_omega_j[i]) / array_of_condition_probabilities_omega_j[i]))
-        )
-    return result
+    return np.sqrt(np.sum(part_0)), np.sqrt(np.sum(part_1))
 
 
-def calculate_binary_m(array_of_condition_probabilities_omega_0, array_of_condition_probabilities_omega_1):
-    size = array_of_condition_probabilities_omega_0.size
-    result_m0 = np.zeros(size)
-    result_m1 = np.zeros(size)
-    wlj_array = calculate_w_lj_coefficients_array(array_of_condition_probabilities_omega_1,
-                                                  array_of_condition_probabilities_omega_0)
+def calc_M(cond_probs_array_0, cond_probs_array_1):
+    size = cond_probs_array_0.size
+
+    m0_part = np.zeros(size)
+    m1_part = np.zeros(size)
+
+    wlj_array = calc_Wlj_coef_arr(cond_probs_array_1,
+                                  cond_probs_array_0)
     for i in range(0, size, 1):
-        result_m0[i] = wlj_array[i] * array_of_condition_probabilities_omega_0[i]
+        m0_part[i] = wlj_array[i] * cond_probs_array_0[i]
 
-        result_m1[i] = wlj_array[i] * array_of_condition_probabilities_omega_1[i]
+        m1_part[i] = wlj_array[i] * cond_probs_array_1[i]
 
-    return np.sum(result_m0), np.sum(result_m1)
+    return np.sum(m0_part), np.sum(m1_part)
 
 
-def calculate_lambda_tilda(P_omega_l, P_omega_j, array_of_condition_probabilities_omega_l,
-                           array_of_condition_probabilities_omega_j):
-    # shape is (81)
-    shape = np.shape(array_of_condition_probabilities_omega_l)
-    pre_result = np.zeros(shape)
+# =============================
+# Байесовский классификатор (обычный и для массива)
+# =============================
+def classify_vectors_array(vectors_array, Pl, Pj, cond_probs_array_l, cond_pobs_array_j):
+    shape   = np.shape(vectors_array)
+    result  = np.zeros(shape[0], int)
+
     for i in range(0, shape[0], 1):
-        pre_result[i] = math.log((1 - array_of_condition_probabilities_omega_l[i]) /
-                                 (1 - array_of_condition_probabilities_omega_j[i]))
-
-    return math.log(P_omega_j / P_omega_l) + np.sum(pre_result)
-
-
-def calculate_Lambda_tilda(X_vector, array_of_condition_probabilities_omega_l,
-                           array_of_condition_probabilities_omega_j):
-    array_wlj = calculate_w_lj_coefficients_array(array_of_condition_probabilities_omega_l,
-                                                  array_of_condition_probabilities_omega_j)
-
-    return np.sum(X_vector * array_wlj)
-
-
-def classify_array_of_vectors(array_of_vectors, P_omega_l, P_omega_j, array_of_condition_probabilities_omega_l,
-                              array_of_condition_probabilities_omega_j):
-    shape = np.shape(array_of_vectors)
-    result = np.zeros(shape[0], int)
-    for i in range(0, shape[0], 1):
-        result[i] = binary_Bayes_classificator(array_of_vectors[i], P_omega_l, P_omega_j,
-                                               array_of_condition_probabilities_omega_l,
-                                               array_of_condition_probabilities_omega_j)
+        result[i] = classify_Bayes(vectors_array[i], Pl, Pj, cond_probs_array_l, cond_pobs_array_j)
 
     return result
 
 
-def binary_Bayes_classificator(X_vector, P_omega_l, P_omega_j, array_of_condition_probabilities_omega_l,
-                               array_of_condition_probabilities_omega_j):
-    Lamda_tilda = calculate_Lambda_tilda(X_vector, array_of_condition_probabilities_omega_l,
-                                         array_of_condition_probabilities_omega_j)
-    lambda_tilda = calculate_lambda_tilda(P_omega_l, P_omega_j, array_of_condition_probabilities_omega_l,
-                                          array_of_condition_probabilities_omega_j)
-    if Lamda_tilda >= lambda_tilda:
-        return int(0)
-    else:
-        return int(1)
+def classify_Bayes(X_v, Pl, Pj, cond_probs_array_l, cond_probs_array_j):
+    Lamda_tilda  = calc_big_lambda(X_v, cond_probs_array_l, cond_probs_array_j)
+    lambda_tilda = calc_small_lambda(Pl, Pj, cond_probs_array_l, cond_probs_array_j)
+    return int(0) if Lamda_tilda >= lambda_tilda else int(1)
 
 
+# =============================
+# Подсчет ошибок
+# =============================
 def calculate_exp_error(classified_array):
     return float(np.sum(classified_array) / classified_array.size)
 
 
-def calculate_theoretical_errors(P_omega_0, P_omega_1, array_of_condition_probabilities_omega_0,
-                                 array_of_condition_probabilities_omega_1):
-    lambda_tilda = calculate_lambda_tilda(P_omega_0, P_omega_1, array_of_condition_probabilities_omega_0,
-                                          array_of_condition_probabilities_omega_1)
-    m0, m1 = calculate_binary_m(array_of_condition_probabilities_omega_0, array_of_condition_probabilities_omega_1)
-    standard_deviation_0, standard_deviation_1 = calculate_binary_SD(array_of_condition_probabilities_omega_0,
-                                                                     array_of_condition_probabilities_omega_1)
-    p0 = 1 - laplas_function((lambda_tilda - m0) / standard_deviation_0)
-    p1 = laplas_function((lambda_tilda - m1) / standard_deviation_1)
-    result = np.array([p0, p1])
+def calculate_theoretical_errors(p0, p1, cond_probs_array_0, cond_probs_array_1):
+    sm_lambda   = calc_small_lambda(p0, p1, cond_probs_array_0, cond_probs_array_1)
+
+    m0, m1      = calc_M(cond_probs_array_0, cond_probs_array_1)
+    sd0, sd1    = calc_binarySD(cond_probs_array_0, cond_probs_array_1)
+
+    p0          = 1 - laplas_function((sm_lambda - m0) / sd0)
+    p1          = laplas_function((sm_lambda - m1) / sd1)
+
+    return np.array([p0, p1])
+
+
+# =============================
+# Переменные промежуточных расчетов
+# =============================
+def calc_Wlj_coef_arr(cond_probs_array_l, cond_probs_array_j):
+    shape = np.shape(cond_probs_array_l)
+    result = np.zeros(shape)
+
+    for i in range(0, shape[0], 1):
+        result[i] = math.log(
+            ((cond_probs_array_l[i] / (1 - cond_probs_array_l[i])) *
+             ((1 - cond_probs_array_j[i]) / cond_probs_array_j[i]))
+        )
     return result
 
+
+def calc_small_lambda(Pl, Pj, cond_probs_array_l, cond_probs_array_j):
+    shape = np.shape(cond_probs_array_l)
+    result_part = np.zeros(shape)
+
+    for i in range(0, shape[0], 1):
+        result_part[i] = math.log((1 - cond_probs_array_l[i]) / (1 - cond_probs_array_j[i]))
+
+    return math.log(Pj / Pl) + np.sum(result_part)
+
+
+def calc_big_lambda(X_vector, cond_probs_array_l, cond_probs_array_j):
+    array_wlj = calc_Wlj_coef_arr(cond_probs_array_l, cond_probs_array_j)
+    return np.sum(X_vector * array_wlj)
