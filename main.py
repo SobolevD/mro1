988a7d6413aaab9b34412ac33e446f6968b8928d
Administrator <admin@example.com>
2022-11-09 22:25:28 +0400

    #2 with Bayes and Fisher implemented

--- main.py
+++ main.py
@@ -61,15 +61,15 @@ if __name__ == '__main__':
     sigma_1     = get_sigma(fisher_W, B0)
     fisher_wN   = get_wN(M0, M1, B0, B0, sigma_0, sigma_1)
 
-    fisher_X1   = np.arange(-3, 3, 0.01)
-    fisher_X0   = get_linear_border(fisher_W, fisher_X1, fisher_wN)
+    fisher_X1_e   = np.arange(-3, 3, 0.01)
+    fisher_X0_e   = get_linear_border(fisher_W, fisher_X1_e, fisher_wN)
 
     x_1_bayes = np.linspace(-3, 3, 400)
     x_0_bayes = np.zeros(len(X0[0]))
     for i in range(0, len(X0[0])):
         x_0_bayes[i] = get_bayos_lines(B0, B0, M0, M1, 0.5, 0.5, x_1_bayes[i])
 
-    plt.scatter(fisher_X0, fisher_X1)
+    plt.scatter(fisher_X0_e, fisher_X1_e)
     plt.scatter(X0_e[0], X0_e[1])
     plt.scatter(X1_e[0], X1_e[1])
 
@@ -91,9 +91,20 @@ if __name__ == '__main__':
     sko_W   = get_W_full(fisher_W, fisher_wN)
     SKO_X0  = glb(sko_W, fisher_X1)
 
+    x_1_bayes = np.linspace(-3, 3, 400)
+    x_00_bayes = np.zeros(len(X0[0]))
+    x_01_bayes = np.zeros(len(X0[0]))
+    for i in range(0, len(X0[0])):
+        x_00_bayes[i], x_01_bayes[i] = get_bayos_lines(B0, B1, M0, M1, 0.5, 0.5, x_1_bayes[i])
+
     plt.scatter(SKO_X0, fisher_X1)
+    plt.scatter(fisher_X0, fisher_X1)
     plt.scatter(X0[0], X0[1])
     plt.scatter(X1[0], X1[1])
+
+    plt.scatter(x_00_bayes, x_1_bayes)
+    plt.scatter(x_01_bayes, x_1_bayes)
+
     plt.xlim((-2, 5))
     plt.title("Минимизация ско. Разные кор.матрицы")
     plt.show()
@@ -106,9 +117,18 @@ if __name__ == '__main__':
     sko_W   = get_W_full(fisher_W, fisher_wN)
     SKO_X0  = glb(sko_W, fisher_X1)
 
-    plt.scatter(SKO_X0, fisher_X1)
+    x_1_bayes = np.linspace(-3, 3, 400)
+    x_0_bayes = np.zeros(len(X0[0]))
+    for i in range(0, len(X0[0])):
+        x_0_bayes[i] = get_bayos_lines(B0, B0, M0, M1, 0.5, 0.5, x_1_bayes[i])
+
+    plt.scatter(SKO_X0, fisher_X1_e)
+    plt.scatter(fisher_X0_e, fisher_X1_e)
     plt.scatter(X0_e[0], X0_e[1])
     plt.scatter(X1_e[0], X1_e[1])
+
+    plt.scatter(x_0_bayes, x_1_bayes)
+
     plt.xlim((-2, 5))
     plt.title("Минимизация ско. Равные кор.матрицы")
     plt.show()
