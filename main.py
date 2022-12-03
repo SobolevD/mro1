84097aa05c3029cd4d9335c56999b3b73896acfb
Administrator <admin@example.com>
2022-12-03 19:29:52 +0400

    Lab7 started

--- main.py
+++ main.py
@@ -1,113 +1,278 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 from utils.K_neighbours import K_neighbours_classifier
-from utils.consts import M0, B0, M1, B1
+from utils.consts import M0, B0, M1, B1, M2, B2, B3, M3, M4, B4
 from utils.errors import classification_error_by_params
 from utils.normal import get_dataset, get_normal_vector
 from utils.parzen import parzen_classifier
 
 
-def classify_data(train_datasets, test_datasets, classifier, params):
-
-    entire_results = []
-    for test_sample in test_datasets:
-        intermediate_results = []
-
-        for j in range(test_sample.shape[1]):
-
-            classification_result = classifier(
-                test_sample[:, j],
-                train_datasets,
-                params)
-
-            intermediate_results.append(classification_result)
-
-        entire_results.append(np.array(intermediate_results))
-
-    return np.array(entire_results)
+def calc_dist_for_vectors(vectors, target_vector):
+    return np.apply_along_axis(np.linalg.norm, 1, vectors - target_vector)
 
 
-def get_classification_error_from_class_nums(y_pred, y_test):
+def calc_example_typical_distanse(centers):
+    distances = np.array([])
+    for center in centers:
+        distances = np.append(distances, calc_dist_for_vectors(centers, center).ravel())
+    distances = distances[distances != 0]
+    return 0.5 * distances.sum() / len(distances)
 
-    N0 = np.sum(y_test == 0)
-    N1 = np.sum(y_test == 1)
 
-    y_pred = np.ravel(y_pred)
+def update_labels(x_samples, centers):
+    curr_distances = np.array([])
+    targets = np.array([])
 
-    p0_count = 0
-    p1_count = 0
+    for i, sample in enumerate(x_samples):
+        dist_cont = calc_dist_for_vectors(centers, sample)
+        min_dist_center_index = np.argmin(dist_cont)
+        curr_distances = np.append(curr_distances, dist_cont[min_dist_center_index])
+        targets = np.append(targets, min_dist_center_index)
 
-    for test_class_num, pred_class_num in zip(y_test, y_pred):
+    return curr_distances, targets
 
-        if [test_class_num, pred_class_num] == [0, 1]:
-            p0_count += 1
 
-        if [test_class_num, pred_class_num] == [1, 0]:
-            p1_count += 1
+def maximin_fit_predict(x_samples, typical_dist_func=None):
+    def find_first_center(x_samples):
+        mean_of_vectors = np.mean(x_samples, axis=0)
+        first_center_index = np.argmax(
+            calc_dist_for_vectors(x_samples_copy, mean_of_vectors)
+        )
+        return x_samples[first_center_index]
 
-    return p0_count / N0, p1_count / N1
+    def find_second_center(x_samples, first_center):
+        second_center_index = np.argmax(calc_dist_for_vectors(x_samples, first_center))
+        return x_samples[second_center_index]
+
+    def update_centers(x_samples, curr_distances, centers):
+        curr_distances_indices = np.argsort(curr_distances)
+        candidate_index = curr_distances_indices[-1]
+        candidate = x_samples[candidate_index]
+
+        stats_maximin_dist.append(curr_distances[candidate_index])
+
+        if curr_distances[candidate_index] > typical_dist_func(centers):
+            centers = np.append(centers, [candidate], axis=0)
+
+        return centers
+
+    stats_typical_dist = []
+    stats_maximin_dist = []
+    stats_r = []
+    r = 3
+
+    x_samples_copy = x_samples.copy()
+    targets = np.array([])
+    centers = np.array([find_first_center(x_samples_copy)])
+
+    second_center = find_second_center(x_samples_copy, centers[0])
+    centers = np.append(centers, [second_center], axis=0)
+
+    while True:
+        update_flag = False
+        curr_distances, targets = update_labels(x_samples, centers)
+        centers_prev = centers.copy()
+
+        stats_typical_dist.append(typical_dist_func(centers))
+        stats_r.append(r)
+
+        centers = update_centers(x_samples, curr_distances, centers)
+        curr_distances, targets = update_labels(x_samples, centers)
+
+        r += 1
+
+        if np.array_equal(centers_prev, centers):
+            break
+
+    plt.title("Заивисимость типичного расстояния от номера итерации")
+    plt.plot(stats_r, stats_typical_dist)
+    plt.show()
+    plt.title("Заивисимость максиминного расстояния от номера итерации")
+    plt.plot(stats_r, stats_maximin_dist)
+    plt.show()
+
+    return x_samples_copy, targets, centers
+
+
+def apply_algorithm(x_samples):
+
+    x_samples, targets, centers = maximin_fit_predict(
+        x_samples, calc_example_typical_distanse
+    )
+    res_clusters = {int(t): [] for t in set(targets)}
+    for x, t in zip(x_samples, targets):
+        res_clusters[t].append(x)
+
+    return res_clusters, centers
+
+
+
+
+
+def merge_classes(datasets, shuffle_mode=True):
+    result = None
+    for class_samples in datasets:
+        if result is None:
+            result = class_samples
+            continue
+
+        result = np.concatenate((result, class_samples), axis=0)
+    if shuffle_mode:
+        np.random.shuffle(result)
+    return np.array(result)
+
+
+def add_dataset_to_canvas(X, color):
+    for x in X:
+        x = np.array(x).flatten()
+        plt.scatter(x[0], x[1], color=color)
+
+
+def k_means_fit_predict(x_samples, K):
+    def update_centers(x_samples, targets, centers):
+        clusters = {int(t): [] for t in set(targets)}
+        for x, t in zip(x_samples, targets):
+            clusters[t].append(x)
+
+        return np.array([np.mean(clusters[k], axis=0) for k in clusters])
+
+    centers = x_samples[:K]
+    #     centers = np.array([x_samples[55], x_samples[103], x_samples[10], x_samples[11], x_samples[211]]) # пример для "неправильной" классификации
+    targets = np.array([])
+    r = 2
+    stats = []
+
+    while True:
+        prev_targets = targets.copy()
+        _, targets = update_labels(x_samples, centers)
+        centers_prev = centers.copy()
+        centers = update_centers(x_samples, targets, centers)
+        stats.append((r, sum(prev_targets != targets)))
+        if np.array_equal(centers_prev, centers):
+            break
+        r += 1
+
+    return x_samples, targets, centers, np.array(stats)
+
+
+colors = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "purple",
+    "cyan",
+    "magenta",
+]
+
+
+def show_result(res_clusters):
+    for t in res_clusters:
+        add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
 
 
 if __name__ == '__main__':
 
-    print("===================================")
-    print("========== Метод Парзена ==========")
-    print("===================================")
+    # 1. Смоделировать и изобразить графически обучающие выборки объема N=50 для пяти
+    # нормально распределенных двумерных случайных векторов с заданными математическими
+    # ожиданиями и самостоятельно подобранными корреляционными матрицами, которые обеспечивают
+    # линейную разделимость классов.
+    N = 50
+    #train_dataset0 = get_dataset(get_normal_vector(2, N), M0, B0, N).T
+    #train_dataset1 = get_dataset(get_normal_vector(2, N), M1, B1, N).T
+    #train_dataset2 = get_dataset(get_normal_vector(2, N), M2, B2, N).T
+    #train_dataset3 = get_dataset(get_normal_vector(2, N), M3, B3, N).T
+    #train_dataset4 = get_dataset(get_normal_vector(2, N), M4, B4, N).T
 
-    n = 2
-    train_dataset_len   = 50
-    test_dataset_len    = 100
+    train_dataset0 = np.load('resources/datasets/red_dataset.npy')
+    train_dataset1 = np.load('resources/datasets/blue_dataset.npy')
+    train_dataset2 = np.load('resources/datasets/green_dataset.npy')
+    train_dataset3 = np.load('resources/datasets/yellow_dataset.npy')
+    train_dataset4 = np.load('resources/datasets/purple_dataset.npy')
 
-    train_dataset0  = get_dataset(get_normal_vector(n, train_dataset_len), M0, B0, train_dataset_len)
-    train_dataset1  = get_dataset(get_normal_vector(n, train_dataset_len), M1, B1, train_dataset_len)
-    train_datasets  = [train_dataset0, train_dataset1]
+    add_dataset_to_canvas(train_dataset0, "red")
+    add_dataset_to_canvas(train_dataset1, "blue")
+    add_dataset_to_canvas(train_dataset2, "green")
+    add_dataset_to_canvas(train_dataset3, "yellow")
+    add_dataset_to_canvas(train_dataset4, "purple")
 
-    test_dataset0   = get_dataset(get_normal_vector(n, test_dataset_len), M0, B0, test_dataset_len)
-    test_dataset1   = get_dataset(get_normal_vector(n, test_dataset_len), M1, B1, test_dataset_len)
-    test_datasets   = [test_dataset0, test_dataset1]
+    plt.show()
 
-    class_num       = np.concatenate((np.zeros(train_dataset_len), np.ones(train_dataset_len)), axis=0)
-    test_class_num  = np.concatenate((np.zeros(test_dataset_len), np.ones(test_dataset_len)), axis=0)
+    # 2. Объединить пять выборок в одну. Общее количество векторов в объединенной выборке должно быть 250.
+    # Полученная объединенная выборка используется для выполнения пунктов 3 и 4 настоящего плана.
+    merged_train_dataset = merge_classes(
+        [train_dataset0, train_dataset1, train_dataset2, train_dataset3, train_dataset4],
+        False
+    )
 
-    y_predicted     = classify_data(train_datasets, test_datasets, parzen_classifier, None)
-    p0, p1          = get_classification_error_from_class_nums(y_predicted, test_class_num)
-    empirical_risk  = 0.5 * p0 + 0.5 * p1
+    add_dataset_to_canvas(merged_train_dataset, "purple")
 
-    print(f"Вероятность ошибочной классификации объекта класса 0 в класс 1: {p0}")
-    print(f"Вероятность ошибочной классификации объекта класса 1 в класс 0: {p1}")
-    print(f"Эмпирический риск: {empirical_risk}")
+    plt.show()
 
-    K_array = [1, 3, 5]
-    for K in K_array:
+    # 3. Разработать программу кластеризации данных с использованием минимаксного алгоритма.
+    # В качестве типичного расстояния взять половину среднего расстояния между существующими кластерами.
+    # Построить отображение результатов кластеризации для числа кластеров, начиная с двух.
+    # Построить график зависимости максимального (из минимальных) и типичного расстояний от числа кластеров.
+    res_clusters, centers_2 = apply_algorithm(merge_classes([train_dataset4, train_dataset2]))
+    res_clusters, centers_3 = apply_algorithm(merge_classes([train_dataset4, train_dataset2, train_dataset3]))
+    res_clusters, centers_4 = apply_algorithm(merge_classes([train_dataset4, train_dataset2, train_dataset3, train_dataset0]))
+    res_clusters, centers_5 = apply_algorithm(merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0]))
 
-        print("===================================")
-        print("==== Метод K ближайшик соседей ====")
-        print(f"============= K = {K} =============")
-        print("===================================")
+    plt.plot(
+        list(range(2, 6)),
+        [
+            calc_example_typical_distanse(centers_2),
+            calc_example_typical_distanse(centers_3),
+            calc_example_typical_distanse(centers_4),
+            calc_example_typical_distanse(centers_5),
+        ],
+    )
+    plt.show()
 
-        params_for_classifier   = [K, class_num]
+    # 4. Разработать программу кластеризации данных с использованием алгоритма К внутригрупповых
+    # средних для числа кластеров равного 3 и 5. Для ситуации 5 кластеров подобрать начальные
+    # условия так, чтобы получить два результата: а) чтобы кластеризация максимально соответствовал
+    # первоначальному разбиению на классы (“правильная” кластеризация); б) чтобы кластеризация
+    # максимально не соответствовала первоначальному разбиению на классы (“неправильная” кластеризация).
+    # Для всех случаев построить графики зависимости числа векторов признаков, сменивших номер кластера,
+    # от номера итерации алгоритма.
 
-        y_predicted             = classify_data(
-            train_datasets,
-            test_datasets,
-            K_neighbours_classifier,
-            params_for_classifier)
+    # Результат работы для 3-х кластеров
+    samples = apply_algorithm(merge_classes([train_dataset4, train_dataset2, train_dataset3]))
+    samples, targets, centers, stats = k_means_fit_predict(samples, 3)
 
-        p0, p1                  = get_classification_error_from_class_nums(y_predicted, test_class_num)
-        empirical_risk          = 0.5 * p0 + 0.5 * p1
+    res_clusters = {int(t): [] for t in set(targets)}
+    for x, t in zip(samples, targets):
+        res_clusters[t].append(x)
 
-        print(f"Вероятность ошибочной классификации объекта класса 0 в класс 1: {p0}")
-        print(f"Вероятность ошибочной классификации объекта класса 1 в класс 0: {p1}")
-        print(f"Эмпирический риск: {empirical_risk}")
+    for t in res_clusters:
+        add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
 
-    print("==================================")
-    print("====== Классификатор Байеса ======")
-    print("==================================")
+    plt.plot(stats[:, 0], stats[:, 1])
 
-    p0              = classification_error_by_params(test_dataset0, M0, M1, B0, B1, 0.5, 0.5)
-    p1              = classification_error_by_params(test_dataset1, M1, M0, B1, B0, 0.5, 0.5)
-    empirical_risk  = 0.5 * p0 + 0.5 * p1
+    # Результат работы для 5-х кластеров (“правильная” кластеризация)
+    samples = merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0])
+    samples, targets, centers, stats = k_means_fit_predict(samples, 5)
 
-    print(f"Вероятность ошибочной классификации объекта класса 0 в класс 1: {p0}")
-    print(f"Вероятность ошибочной классификации объекта класса 1 в класс 0: {p1}")
-    print(f"Эмпирический риск: {empirical_risk}")
+    res_clusters = {int(t): [] for t in set(targets)}
+    for x, t in zip(samples, targets):
+        res_clusters[t].append(x)
+
+    for t in res_clusters:
+        add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
+
+    plt.plot(stats[:, 0], stats[:, 1])
+
+    # Результат работы для 5-х кластеров (“неправильная” кластеризация)
+    samples = merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0])
+    samples, targets, centers, stats = k_means_fit_predict(samples, 5)
+
+    res_clusters = {int(t): [] for t in set(targets)}
+    for x, t in zip(samples, targets):
+        res_clusters[t].append(x)
+
+    for t in res_clusters:
+        add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
+
+    plt.plot(stats[:, 0], stats[:, 1])
