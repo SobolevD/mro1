fa802492820965c71c10d8bcac51d128c007f697
Administrator <admin@example.com>
2022-12-08 08:35:44 +0400

    Some fixes

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from utils.consts import M0, B1, M1, B2, M2, B3, M3, B4, M4, B0
-from utils.normal import get_dataset, get_normal_vector
-
 
 def calc_dist_for_vectors(vectors, target_vector):
     return np.apply_along_axis(np.linalg.norm, 1, vectors - target_vector)
 
 
-def calc_example_typical_distanse(centers):
+def calc_typical_distanse(centers):
     distances = np.array([])
     for center in centers:
         distances = np.append(distances, calc_dist_for_vectors(centers, center).ravel())
@@ -17,12 +14,12 @@ def calc_example_typical_distanse(centers):
     return 0.5 * distances.sum() / len(distances)
 
 
-def update_labels(x_samples, centers):
+def update_labels(dataset, centers):
     curr_distances = np.array([])
     targets = np.array([])
 
-    for i, sample in enumerate(x_samples):
-        dist_cont = calc_dist_for_vectors(centers, sample)
+    for i, element in enumerate(dataset):
+        dist_cont = calc_dist_for_vectors(centers, element)
         min_dist_center_index = np.argmin(dist_cont)
         curr_distances = np.append(curr_distances, dist_cont[min_dist_center_index])
         targets = np.append(targets, min_dist_center_index)
@@ -30,22 +27,22 @@ def update_labels(x_samples, centers):
     return curr_distances, targets
 
 
-def maximin_fit_predict(x_samples, typical_dist_func=None):
-    def find_first_center(x_samples):
-        mean_of_vectors = np.mean(x_samples, axis=0)
+def maximin_predict(dataset, typical_dist_func=None):
+    def find_first_center(dataset):
+        mean_of_vectors = np.mean(dataset, axis=0)
         first_center_index = np.argmax(
-            calc_dist_for_vectors(x_samples_copy, mean_of_vectors)
+            calc_dist_for_vectors(dataset_copy, mean_of_vectors)
         )
-        return x_samples[first_center_index]
+        return dataset[first_center_index]
 
-    def find_second_center(x_samples, first_center):
-        second_center_index = np.argmax(calc_dist_for_vectors(x_samples, first_center))
-        return x_samples[second_center_index]
+    def find_second_center(dataset, first_center):
+        second_center_index = np.argmax(calc_dist_for_vectors(dataset, first_center))
+        return dataset[second_center_index]
 
-    def update_centers(x_samples, curr_distances, centers):
+    def update_centers(dataset, curr_distances, centers):
         curr_distances_indices = np.argsort(curr_distances)
         candidate_index = curr_distances_indices[-1]
-        candidate = x_samples[candidate_index]
+        candidate = dataset[candidate_index]
 
         stats_maximin_dist.append(curr_distances[candidate_index])
 
@@ -59,23 +56,22 @@ def maximin_fit_predict(x_samples, typical_dist_func=None):
     stats_r = []
     r = 3
 
-    x_samples_copy = x_samples.copy()
+    dataset_copy = dataset.copy()
     targets = np.array([])
-    centers = np.array([find_first_center(x_samples_copy)])
+    centers = np.array([find_first_center(dataset_copy)])
 
-    second_center = find_second_center(x_samples_copy, centers[0])
+    second_center = find_second_center(dataset_copy, centers[0])
     centers = np.append(centers, [second_center], axis=0)
 
     while True:
-        update_flag = False
-        curr_distances, targets = update_labels(x_samples, centers)
+        curr_distances, targets = update_labels(dataset, centers)
         centers_prev = centers.copy()
 
         stats_typical_dist.append(typical_dist_func(centers))
         stats_r.append(r)
 
-        centers = update_centers(x_samples, curr_distances, centers)
-        curr_distances, targets = update_labels(x_samples, centers)
+        centers = update_centers(dataset, curr_distances, centers)
+        curr_distances, targets = update_labels(dataset, centers)
 
         r += 1
 
@@ -89,16 +85,16 @@ def maximin_fit_predict(x_samples, typical_dist_func=None):
     plt.plot(stats_r, stats_maximin_dist)
     plt.show()
 
-    return x_samples_copy, targets, centers
+    return dataset_copy, targets, centers
 
 
-def apply_algorithm(x_samples):
+def apply_algorithm(dataset):
 
-    x_samples, targets, centers = maximin_fit_predict(
-        x_samples, calc_example_typical_distanse
+    dataset, targets, centers = maximin_predict(
+        dataset, calc_typical_distanse
     )
     res_clusters = {int(t): [] for t in set(targets)}
-    for x, t in zip(x_samples, targets):
+    for x, t in zip(dataset, targets):
         res_clusters[t].append(x)
 
     return res_clusters, centers
@@ -106,32 +102,32 @@ def apply_algorithm(x_samples):
 
 def merge_classes(datasets, shuffle_mode=True):
     result = None
-    for class_samples in datasets:
+    for class_dataset in datasets:
         if result is None:
-            result = class_samples
+            result = class_dataset
             continue
 
-        result = np.concatenate((result, class_samples), axis=0)
+        result = np.concatenate((result, class_dataset), axis=0)
     if shuffle_mode:
         np.random.shuffle(result)
     return np.array(result)
 
 
-def add_dataset_to_canvas(X, color):
-    for x in X:
+def add_dataset_to_canvas(dataset, color):
+    for x in dataset:
         x = np.array(x).flatten()
         plt.scatter(x[0], x[1], color=color)
 
 
-def k_means_fit_predict(x_samples, K):
-    def update_centers(x_samples, targets, centers):
+def k_means_predict(dataset, K):
+    def update_centers(dataset, targets):
         clusters = {int(t): [] for t in set(targets)}
-        for x, t in zip(x_samples, targets):
+        for x, t in zip(dataset, targets):
             clusters[t].append(x)
 
         return np.array([np.mean(clusters[k], axis=0) for k in clusters])
 
-    centers = x_samples[:K]
+    centers = dataset[:K]
     targets = np.array([])
 
     r = 2
@@ -139,9 +135,9 @@ def k_means_fit_predict(x_samples, K):
 
     while True:
         prev_targets = targets.copy()
-        _, targets = update_labels(x_samples, centers)
+        _, targets = update_labels(dataset, centers)
         centers_prev = centers.copy()
-        centers = update_centers(x_samples, targets, centers)
+        centers = update_centers(dataset, targets)
 
         equal = 1 if prev_targets.all() != targets.all() else 0
 
@@ -150,7 +146,7 @@ def k_means_fit_predict(x_samples, K):
             break
         r += 1
 
-    return x_samples, targets, centers, np.array(stats)
+    return dataset, targets, centers, np.array(stats)
 
 
 colors = [
@@ -175,7 +171,6 @@ if __name__ == '__main__':
     # нормально распределенных двумерных случайных векторов с заданными математическими
     # ожиданиями и самостоятельно подобранными корреляционными матрицами, которые обеспечивают
     # линейную разделимость классов.
-    N = 50
 
     train_dataset0 = np.load('resources/datasets/red_dataset.npy')
     train_dataset1 = np.load('resources/datasets/blue_dataset.npy')
@@ -199,7 +194,6 @@ if __name__ == '__main__':
     )
 
     add_dataset_to_canvas(merged_train_dataset, "purple")
-
     plt.show()
 
     # 3. Разработать программу кластеризации данных с использованием минимаксного алгоритма.
@@ -210,7 +204,7 @@ if __name__ == '__main__':
 
     add_dataset_to_canvas(train_dataset0, "red")
     add_dataset_to_canvas(train_dataset4, "purple")
-    plt.scatter(centers_2[:2, 0], centers_2[:2, 1], color='black', s=100)
+    #plt.scatter(centers_2[:2, 0], centers_2[:2, 1], color='black', s=100)
     plt.show()
 
     res_clusters, centers_3 = apply_algorithm(merge_classes([train_dataset0, train_dataset4, train_dataset3]))
@@ -218,7 +212,7 @@ if __name__ == '__main__':
     add_dataset_to_canvas(train_dataset0, "red")
     add_dataset_to_canvas(train_dataset4, "purple")
     add_dataset_to_canvas(train_dataset3, "yellow")
-    plt.scatter(centers_3[:3, 0], centers_3[:3, 1], color='black', s=100)
+    #plt.scatter(centers_3[:3, 0], centers_3[:3, 1], color='black', s=100)
     plt.show()
 
     res_clusters, centers_4 = apply_algorithm(merge_classes([train_dataset0, train_dataset4, train_dataset3, train_dataset2]))
@@ -227,7 +221,7 @@ if __name__ == '__main__':
     add_dataset_to_canvas(train_dataset4, "purple")
     add_dataset_to_canvas(train_dataset3, "yellow")
     add_dataset_to_canvas(train_dataset2, "green")
-    plt.scatter(centers_4[:4, 0], centers_4[:4, 1], color='black', s=100)
+    #plt.scatter(centers_4[:4, 0], centers_4[:4, 1], color='black', s=100)
     plt.show()
 
     res_clusters, centers_5 = apply_algorithm(merge_classes([train_dataset0, train_dataset4, train_dataset3, train_dataset2, train_dataset1]))
@@ -237,17 +231,17 @@ if __name__ == '__main__':
     add_dataset_to_canvas(train_dataset3, "yellow")
     add_dataset_to_canvas(train_dataset2, "green")
     add_dataset_to_canvas(train_dataset1, "blue")
-    plt.scatter(centers_5[:5, 0], centers_5[:5, 1], color='black', s=100)
+    #plt.scatter(centers_5[:5, 0], centers_5[:5, 1], color='black', s=100)
     plt.show()
 
     plt.title("Зависимость макс. (из мин.) и типичного расстояний от числа кластеров", fontsize=10)
     plt.plot(
         list(range(2, 6)),
         [
-            calc_example_typical_distanse(centers_2),
-            calc_example_typical_distanse(centers_3),
-            calc_example_typical_distanse(centers_4),
-            calc_example_typical_distanse(centers_5),
+            calc_typical_distanse(centers_2),
+            calc_typical_distanse(centers_3),
+            calc_typical_distanse(centers_4),
+            calc_typical_distanse(centers_5),
         ],
     )
     plt.show()
@@ -260,18 +254,18 @@ if __name__ == '__main__':
     # Для всех случаев построить графики зависимости числа векторов признаков, сменивших номер кластера,
     # от номера итерации алгоритма.
 
-    # Результат работы для 3-х кластеров
-    x_samples = merge_classes([train_dataset4, train_dataset2, train_dataset3])
-    x_samples, targets, centers, stats = k_means_fit_predict(x_samples, 3)
+    # 3 КЛАСТЕРА
+    dataset = merge_classes([train_dataset4, train_dataset2, train_dataset3])
+    dataset, targets, centers, stats = k_means_predict(dataset, 3)
 
     res_clusters = {int(t): [] for t in set(targets)}
-    for x, t in zip(x_samples, targets):
+    for x, t in zip(dataset, targets):
         res_clusters[t].append(x)
 
     for t in res_clusters:
         add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
 
-    plt.scatter(centers[:, 0], centers[:, 1], color='black', s=100)
+    #plt.scatter(centers[:, 0], centers[:, 1], color='black', s=100)
     plt.show()
 
     plt.title("Зависимость числа векторов признаков, сменивших номер кластера, от номера итерации алгоритма", fontsize=8)
@@ -279,17 +273,17 @@ if __name__ == '__main__':
     plt.show()
 
     # Результат работы для 5-х кластеров (“правильная” кластеризация)
-    x_samples = merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0])
-    x_samples, targets, centers, stats = k_means_fit_predict(x_samples, 5)
+    dataset = merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0])
+    dataset, targets, centers, stats = k_means_predict(dataset, 5)
 
     res_clusters = {int(t): [] for t in set(targets)}
-    for x, t in zip(x_samples, targets):
+    for x, t in zip(dataset, targets):
         res_clusters[t].append(x)
 
     for t in res_clusters:
         add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
 
-    plt.scatter(centers[:, 0], centers[:, 1], color='black', s=100)
+    #plt.scatter(centers[:, 0], centers[:, 1], color='black', s=100)
     plt.show()
 
     plt.title("Зависимость числа векторов признаков, сменивших номер кластера, от номера итерации алгоритма", fontsize=8)
@@ -297,17 +291,17 @@ if __name__ == '__main__':
     plt.show()
 
     # Результат работы для 5-х кластеров (“неправильная” кластеризация)
-    x_samples = merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0])
-    x_samples, targets, centers, stats = k_means_fit_predict(x_samples, 5)
+    dataset = merge_classes([train_dataset4, train_dataset2, train_dataset1, train_dataset3, train_dataset0])
+    dataset, targets, centers, stats = k_means_predict(dataset, 5)
 
     res_clusters = {int(t): [] for t in set(targets)}
-    for x, t in zip(x_samples, targets):
+    for x, t in zip(dataset, targets):
         res_clusters[t].append(x)
 
     for t in res_clusters:
         add_dataset_to_canvas(np.matrix(res_clusters[t]), colors[t])
 
-    plt.scatter(centers[:, 0], centers[:, 1], color='black', s=100)
+    #plt.scatter(centers[:, 0], centers[:, 1], color='black', s=100)
     plt.show()
 
     plt.title("Зависимость числа векторов признаков, сменивших номер кластера, от номера итерации алгоритма", fontsize=8)
